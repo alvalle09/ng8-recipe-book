8050e9b69864e14316620cd3755779c51b994569
alvalle09 <phredvalle@gmail.com>
2019-10-07 22:38:49 -0500

    add httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { ServerComponent } from './server/server.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
