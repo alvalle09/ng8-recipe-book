9b79b62c6c0da2f85d029736085da2e7b0299911
alvalle09 <phredvalle@gmail.com>
2019-10-14 23:03:45 -0500

    add ng directives for ngClass, ngStyle, ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,11 +10,12 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Test Server';
   serverCreated = false;
+  servers = ['TestServer1', 'TestServer2'];
 
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 3000);
+    }, 2000);
   }
 
   ngOnInit() {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;  
   }
 
