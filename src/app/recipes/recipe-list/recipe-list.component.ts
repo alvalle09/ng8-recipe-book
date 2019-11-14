5877ff4c5ca94fae8b0a485c554b64fb66d6a021
alvalle09 <phredvalle@gmail.com>
2019-11-13 23:31:33 -0600

    formatting fixes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
+
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html",
   styleUrls: ["./recipe-list.component.css"]
 })
+
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
@@ -13,9 +15,14 @@ export class RecipeListComponent implements OnInit {
       "https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false"
     ),
     new Recipe(
-      "A Test Recipe",
-      "This is simply test",
+      "Another Recipe",
+      "This is sweet recipe",
       "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"
+    ),
+    new Recipe(
+      "Super Recipe",
+      "This is super recipe",
+      "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
     )
   ];
 
