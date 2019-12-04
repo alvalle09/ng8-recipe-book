85ee4f6dfb8df273aefd2013d8875f08e2192463
alvalle09 <phredvalle@gmail.com>
2019-12-04 00:22:39 -0600

    setup recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit , EventEmitter, Output} from "@angular/core";
 import { Recipe } from "../recipe.model";
+
 
 @Component({
   selector: "app-recipe-list",
@@ -8,6 +9,7 @@ import { Recipe } from "../recipe.model";
 })
 
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       "A Test Recipe",
@@ -29,5 +31,10 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
+  }
 }
 
