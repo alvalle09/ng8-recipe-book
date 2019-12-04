63ee2415ee49a9ce791839ce40c55a055aedeaf4
alvalle09 <phredvalle@gmail.com>
2019-12-03 23:18:59 -0600

    create recipe item component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
-
 })
 
 export class HeaderComponent {
