4c865b1f300dc5e9134d04a1cc1fc695de8139cc
alvalle09 <phredvalle@gmail.com>
2021-02-10 21:51:53 -0600

    first method to access recipe list

--- src/app/Shared/data-storage.service.ts
+++ src/app/Shared/data-storage.service.ts
@@ -1,7 +1,14 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
+import { Recipe } from '../recipes/recipe.model';
 
 // optional syntax to avoid setting up in app module providers array
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor()
+    constructor(private http:HttpClient) {}
+
+    storeRecipes(recipes: Recipe[]) {
+
+    }
 }
