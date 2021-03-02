f331e148603311d2f97ea2b7edcd7cda34f47671
alvalle09 <phredvalle@gmail.com>
2021-03-02 00:02:58 -0600

    use resolver service to handle page refresh and no data loaded yet

--- src/app/Shared/data-storage.service.ts
+++ src/app/Shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 // optional syntax to avoid setting up in app module providers array
 @Injectable({providedIn: 'root'})
@@ -20,16 +20,21 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>('https://ng-complete-guide-d4625-default-rtdb.firebaseio.com/recipes.json')
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []};
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe, 
+                            ingredients: recipe.ingredients ? recipe.ingredients: []
+                        };
+                    });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes)
                 })
-            }))
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
-            });
+            )
     }
 }
 
