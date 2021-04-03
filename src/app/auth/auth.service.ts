cf618971ee005bff6df43f4c02c04f41ac0e1ba6
alvalle09 <phredvalle@gmail.com>
2021-04-02 23:50:47 -0500

    setup firebase apikey request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,10 +5,10 @@ import { Injectable } from "@angular/core";
 @Injectable({providedIn: 'root'})
 export class AuthService {
     constructor(private http: HttpClient) {
-        
+
     }
 
     signup() {
-
+        this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]');
     }
 }
