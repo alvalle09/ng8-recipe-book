88ebe8415a3a81c02d9a7e8f48b0ad495ec7bbfd
alvalle09 <phredvalle@gmail.com>
2021-04-08 00:15:55 -0500

    setup api call for signup mode

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,41 @@
 import { Component } from "@angular/core";
-import { NgForm } from '@angular/forms';
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
-    selector: 'app-auth',
-    templateUrl: './auth.component.html'
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
 })
-
 export class AuthComponent {
-    isLoginMode = true;
+  isLoginMode = true;
 
-    onSwitchMode() {
-       this.isLoginMode = !this.isLoginMode; 
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.value) {
+      return;
     }
-    
-    onSubmit(form: NgForm) {
-        console.log(form.value);
-        form.reset();
+    //console.log(form.value);
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+        // todo..
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
+
+    form.reset();
+  }
 }
