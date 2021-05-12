c7541ac1a59fbc1b2a4bcf04757bfab3aa3e68ab
alvalle09 <phredvalle@gmail.com>
2021-05-11 22:41:25 -0500

    create errorHandler for sharing between login and signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from 'rxjs';
@@ -21,25 +21,14 @@ export class AuthService {
     signup(email: string, password: string) {
         console.log(email);
         return this.http.post<AuthResponseData>
-        ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=**************************',
-        {
-            email: email, 
-            password: password, 
-            returnSecureToken: true
-        }
-        )
-        .pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown erro occured';
-            if (!errorRes.error || !errorRes.error.error ) {
-                return throwError(errorMessage);
-            };
-            
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                  errorMessage = 'This email exists already';
+            ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=**************************',
+            {
+                email: email, 
+                password: password, 
+                returnSecureToken: true
             }
-            return throwError(errorMessage);
-        }));
+        )
+        .pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string) {
@@ -50,7 +39,21 @@ export class AuthService {
                     password: password, 
                     returnSecureToken: true
                 }
-            );            
+            )
+            .pipe(catchError(this.handleError));            
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown erro occured';
+        if (!errorRes.error || !errorRes.error.error ) {
+            return throwError(errorMessage);
+        };
+        
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
     }
     
 }
