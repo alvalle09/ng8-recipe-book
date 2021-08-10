180ddbd923655c6c1efebe1971381d93fd68e878
alvalle09 <phredvalle@gmail.com>
2021-08-09 23:49:28 -0500

    unsubscribe from dynamically created component subscriptions, add space to buttons on login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
@@ -9,9 +9,10 @@ import { AuthResponseData, AuthService } from "./auth.service";
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
+  styleUrls: ['./auth.component.css']
 })
 
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -69,6 +70,12 @@ export class AuthComponent {
 
   onHandleError() {
     this.error = null;
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 
   private showErrorAlert(message: string) {
